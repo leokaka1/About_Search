fa3582829b9be58aca28cc67063c416b988b19b4
Leon Cao <leocaoxiaozhu@163.com>
2021-08-10 19:44:13 +0800

    修改了删除coos多余词的方法
    修改了删除coos多余词的方法

--- Search_Demo_1/sematicSetting.py
+++ Search_Demo_1/sematicSetting.py
@@ -42,8 +42,6 @@ def firstSituation(analysisModel: SematicAnalysisModel):
     # 先拷贝最终返回数组为名词数组
     final_sequence_word_list = analysisModel.posModel.nouns.copy()
 
-    print(analysisModel.posModel.coosHasWords)
-
     # 不包含属性
     if not analysisModel.posModel.attriHasWords:
         # 不包含并列关系
