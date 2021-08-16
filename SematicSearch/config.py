362a7dca6798eef56055c61870f9008a74507d72
Leon Cao <leocaoxiaozhu@163.com>
2021-08-16 11:03:33 +0800

    增加resources并且增加了config
    增加resources并且增加了config

---
+++ SematicSearch/config.py
@@ -0,0 +1,12 @@
+import os
+
+# 当前目录
+current_path = os.getcwd() + '\\resources'
+
+# 属性关系列表
+attribute_path = current_path + "\\attributes"
+disambiguration_path = current_path + "\\disambiguration"
+entities_path = current_path + "\\entities"
+relations_path = current_path + "\\relations"
+types_path = current_path + "\\types"
+user_dicts = current_path + "\\user_dicts"
---
+++ SematicSearch/main.py
@@ -0,0 +1,4 @@
+from SematicSearch.config import *
+
+if __name__ == '__main__':
+    print(attribute_path)
