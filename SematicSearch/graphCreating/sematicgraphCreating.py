0d84f8de21347b662af400096da40bef03b91b02
Leon Cao <leocaoxiaozhu@163.com>
2021-08-18 11:26:55 +0800

    添加了属性值此的index

--- SematicSearch/graphCreating/sematicgraphCreating.py
+++ SematicSearch/graphCreating/sematicgraphCreating.py
@@ -75,7 +75,7 @@ def seperatingTypeOfWords(segAndPostList):
         # 处理属性值操作
         for index, posWord in enumerate(sematic_Model.pos_list):
             if posWord == "TIME" or posWord == "m" or posWord == "PER":
-                valueList.append(sematic_Model.word_list[index])
+                valueList.append(combineWords(sematic_Model.word_list[index],index))
 
         if coo_list:
             coo_list = set(coo_list)
